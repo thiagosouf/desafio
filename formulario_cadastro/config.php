2505bb882b7d4261a7916595d552adc62cea32b8
Thiago Ferreira <62024096+thiagosouf@users.noreply.github.com>
2021-10-27 21:37:38 -0300

    atualizacoes

---
+++ formulario_cadastro/config.php
@@ -0,0 +1,51 @@
+<?php
+
+    $dbHost = "localhost";
+    $dbUsername = "root";
+    $dbPassword = '';
+    $dbName = "usuarios";
+
+    $conexao = new mysqli($dbHost,$dbUsername,$dbPassword,$dbName);
+    // $conexao = new mysqli("localhost","root","","usuarios");
+    
+    $nome = $_POST["fname"];
+    $cpf = $_POST["cpf"];
+    $email=$_POST["email"];
+    $telefone=$_POST["telefone"];
+    $sexo=$_POST["sexo"];
+    $nasc=$_POST["nascimento"];
+    $cidade=$_POST["cidade"];
+    $estado=$_POST["estado"];
+    $hoje = date('d/m/Y');
+
+    $result = mysqli_query($conexao, "INSERT INTO cadastro (Nome,CPF,Email,Telefone,Sexo,DtNascimento,Cidade,Estado,outros) 
+    VALUES ('$nome', '$cpf', '$email', '$telefone', '$sexo', '$nasc', '$cidade', '$estado', '$hoje')");
+    //colocar aspas simples nas variaveis
+
+
+    
+
+
+    // TESTES
+    // if($conexao == TRUE){
+    //     echo "SUCESSO";
+    // }
+    // else{
+    //     echo "ERRO";
+    // }
+
+    // var_dump($nome);
+    // print_r("<br>");
+    // var_dump($cpf);
+    // print_r("<br>");
+    // var_dump($email);
+    // print_r("<br>");
+    // var_dump($telefone);
+    // print_r("<br>");
+    // var_dump($sexo);
+    // print_r("<br>");
+    // var_dump($nasc);
+    // print_r("<br>");
+    // var_dump($cidade);
+    // print_r("<br>");
+    // var_dump($estado);
